5b27729ea3e68a476291e1f8e80864f4015dc544
lev <kueappaeuk@gmail.com>
2024-10-22 18:16:05 +0300

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Радченко Лев";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
